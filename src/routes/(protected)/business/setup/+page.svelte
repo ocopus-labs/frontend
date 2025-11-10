59959fe3abd24f1d48b8a82ca6458c0d262b56cd
Rohit Kushwaha <technicalrohit06@gmail.com>
2025-11-10 23:15:21 +0530

    feat(business): add SearchSelect component for award selection in business setup

---
+++ src/routes/(protected)/business/setup/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	import SearchSelect from '$lib/components/global/search-select.svelte';
+
+	let value = $state('');
+
+	let options = $state([
+		{ label: 'Option 1', value: 'option1' },
+		{ label: 'Option 2', value: 'option2' },
+		{ label: 'Option 3', value: 'option3' }
+	]);
+</script>
+
+<SearchSelect
+	bind:value
+	{options}
+	placeholder="Select an awards"
+	emptyPlaceholder="No awards found"
+/>
